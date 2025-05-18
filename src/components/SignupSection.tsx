19944b2a3b134764febf3a47a93412f81b022b94
Mingxuan Sun <57325532+SunMingxuanSher@users.noreply.github.com>
2025-05-17 21:25:18 -0400

    Update SignupSection.tsx

--- src/components/SignupSection.tsx
+++ src/components/SignupSection.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { useToast } from "@/components/ui/use-toast";
-import { saveEmailAsJson } from 'emailSaver';
+import { saveEmailAsJson } from './emailSaver';
 
 const SignupSection = () => {
   const { toast } = useToast();
